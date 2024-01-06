72643fb2e621fb519efaa09e2541438a8caee779
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-01-06 08:36:10 +0300

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to the Photo Tagging Game!</h1>
+      <div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
