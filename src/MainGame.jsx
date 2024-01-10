9c9814084b22c6b24acede0e521fec3df23d3b0b
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-01-10 06:14:21 +0300

    Use Canvas API for the image

--- src/MainGame.jsx
+++ src/MainGame.jsx
@@ -1,12 +1,25 @@
-import Image from "./Image";
+import { useRef, useEffect } from "react";
 
 const MainGame = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    const image = new Image();
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx.drawImage(image, 0, 0)
+    };
+
+    image.src = "src/assets/photo-tagging-main.jpg";
+  }, [canvasRef]);
+
   return (
     <div className="main-game">
-      <Image
-        name={"photo-tagging-main.jpg"}
-        altname={"Picture crowded with characters"}
-      />
+      <canvas ref={canvasRef}></canvas>
     </div>
   );
 };
