85f4e7e25c21cb11c5c5c578daf14a5dfef5f246
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-01-11 06:58:18 +0300

    Map characters and their locations

--- src/MainGame.jsx
+++ src/MainGame.jsx
@@ -3,6 +3,14 @@ import { useRef, useEffect } from "react";
 const MainGame = () => {
   const canvasRef = useRef(null);
 
+  const characters = {
+    "Washington Carver": { ratio: [7.75, 64.90], coords: [] },
+    "Einstein": { ratio: [13.93, 38.70], coords: [] },
+    "Curie": { ratio: [30.55, 75.59], coords: [] },
+    "Newton": { ratio: [91.84, 87.63], coords: [] },
+    "Ride": { ratio: [57.66, 34.17], coords: [] },
+  };
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
@@ -11,15 +19,30 @@ const MainGame = () => {
     image.onload = () => {
       canvas.width = image.width;
       canvas.height = image.height;
-      ctx.drawImage(image, 0, 0)
-    };
+      ctx.drawImage(image, 0, 0);
 
+      // Calculate coordinates based on ratios and canvas size
+      Object.keys(characters).forEach((character) => {
+        const [xRatio, yRatio] = characters[character].ratio;
+        const xCoord = (xRatio / 100) * canvas.clientWidth;
+        const yCoord = (yRatio / 100) * canvas.clientHeight;
+
+        characters[character].coords = [xCoord, yCoord];
+      });
+    };
     image.src = "src/assets/photo-tagging-main.jpg";
-  }, [canvasRef]);
+  });
+
+  const handleClick = (event) => {
+    // I'm using this to confirm location of the characters
+    const bounding = canvasRef.current.getBoundingClientRect();
+    const x = event.clientX - bounding.left;
+    const y = event.clientY - bounding.top;
+  };
 
   return (
     <div className="main-game">
-      <canvas ref={canvasRef}></canvas>
+      <canvas ref={canvasRef} onClick={(e) => handleClick(e)}></canvas>
     </div>
   );
 };
