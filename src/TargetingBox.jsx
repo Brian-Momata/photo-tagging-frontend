9b944316147498992fb0985ea02cda66364166c8
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-01-15 07:15:10 +0300

    Add dropdown to select character

--- src/TargetingBox.jsx
+++ src/TargetingBox.jsx
@@ -1,11 +1,41 @@
+import { useState } from "react"
+
+const Dropdown = () => {
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+
+  const handleSelectionChange = (e) => {
+    setSelectedCharacter(() => e.target.value);
+  };
+
+  return (
+    <div>
+      <select
+        id="characterDropdown"
+        value={selectedCharacter}
+        onChange={handleSelectionChange}
+        className="character-dropdown"
+      >
+        <option value="">Who is this?</option>
+        <option value="Washington Carver">Washington Carver</option>
+        <option value="Einstein">Einstein</option>
+        <option value="Curie">Curie</option>
+        <option value="Newton">Newton</option>
+        <option value="Ride">Ride</option>
+      </select>
+    </div>
+  )
+}
+
+
 const TargetingBox = ({ left, top}) => {
   const boxStyle = {
-    // subtracting the 20 so that the cursor positions better
-    top: `${top - 20}px`,
-    left: `${left - 20}px`,
+    // subtracting the 10px so that the cursor positions better
+    top: `${top - 10}px`,
+    left: `${left - 10}px`,
   }
   return (
     <div className="targeting-box" style={boxStyle}>
+      < Dropdown />
     </div>
   );
 }
